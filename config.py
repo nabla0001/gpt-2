f5b6701ccd66fe9f256244306423d671503f4e1e
nabla0001 <87380479+nabla0001@users.noreply.github.com>
2024-11-10 22:31:03 +0100

    GPT1 paper config

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ import torch
 class GPTConfig:
     vocab_size: int = 50304
     context_size: int = 512
-    embedding_size: int = 512
-    n_heads: int = 8
-    n_layers: int = 8
+    embedding_size: int = 768
+    n_heads: int = 12
+    n_layers: int = 12
     dropout: float = 0.1
 
 @dataclass
@@ -15,15 +15,15 @@ class Config:
     gpt: GPTConfig = GPTConfig()
     device: str = 'mps'
     dtype: torch.dtype = torch.float16 # 'torch.float16' for mixed precision
-    grad_accumulation_batches: int = 1
-    batch_size: int = 16
-    learning_rate: float = 2.5e-3
+    gradient_accumulation_steps: int = 8
+    batch_size: int = 8
+    learning_rate: float = 2.5e-4
     warmup_iters = 2000
     min_lr = 6e-5
     # beta1: float = 0.9
     # beta2: float = 0.95
-    n_batches: int = 2000
+    n_batches: int = 60000
     log_interval: int = 20
-    checkpoint_interval: int = 2000 # 5000
+    checkpoint_interval: int = 5000
     eval_interval: int = 100
     n_eval_batches: int = 100
