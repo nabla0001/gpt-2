1c7d25fc3cb74bcb9f2e309289ccb74419944550
nabla0001 <87380479+nabla0001@users.noreply.github.com>
2024-11-12 19:00:48 +0100

    update

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
     min_lr = 6e-5
     # beta1: float = 0.9
     # beta2: float = 0.95
-    n_batches: int = 3 # 60000
+    n_batches: int = 600000
     log_interval: int = 20
     eval_interval: int = 100
     n_eval_batches: int = 100
